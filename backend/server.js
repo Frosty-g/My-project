75a7c81c9238ca366ef31fd8e5506c2b74c67e8c
Glaine Amoni <glaineamukhoye@gmail.com>
2025-11-07 16:50:39 +0300

    Changed routes to be able to retrive data

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,26 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from './models/userModels.js';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
 const router = express.Router();
 const SECRET_KEY = "Frosty_Secret";
 
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
+
 app.use("/", router);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://Frosty:Glaineamoni@cluster1.ihwj6th.mongodb.net/?appName=Cluster1')
@@ -20,24 +31,29 @@ mongoose.connect('mongodb+srv://Frosty:Glaineamoni@cluster1.ihwj6th.mongodb.net/
 
 // User Registration
 router.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password } =req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
-    res.json({ message: 'User registered successfully' });
+
+    res.status(201).json({ message: 'User registered successfully', name: newUser.name });
 });
 
 // User Login
 router.post('/login', async (req, res) => {
     const { email, password } =req.body;
     const user = await User.findOne({ email });
-    if (!user) res.status(404)/json({ message: 'User not found' });
+    if (!user) res.status(404).json({ message: 'User not found' });
 
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) return res.json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
-    res.json({ token });
+    res.json({ message: "Login successful", token, name: user.name });
 });
 
 // Protected Route
